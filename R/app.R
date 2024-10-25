2e7939da254eb9255a8fdff1a14f70d3586720d0
Guillermo Garcia-Costoya <ggarciacosto@gmail.com>
2024-10-25 10:32:25 -0700

    final changes to the app before publishing

--- R/app.R
+++ R/app.R
@@ -1,11 +1,27 @@
 
-library(tidyverse)
-
 # define user interface
 user_interface <- fluidPage(
 
   # set application title
-  titlePanel("Exploring the effects of plasticity"),
+  titlePanel("Exploring the effects of thermal plasticity on adaptation to climate change"),
+
+  # add description
+    # add description
+  fluidRow(
+    column(12,
+      wellPanel(
+        p("This application allows you to explore how can phenotypic plasticity in
+          thermal performance curves (TPC) aids adaptation to changing thermal environments.
+          Adjust the thermal optima (Topt) and critical thermal limits (CTmin and CTmax) to
+          shape the TPC. Set an initial temperature distribution (initial mean and standard deviation)
+          and explore how changes in mean and thermal variability (change in SD) impact performance.
+          You can also control the TPC's acclimation response ratio (ARR) which will determine
+          how much do all thermal physiology traits change as a function of changes in mean temperature
+          (e.g., ARR = 0.5 will mean that all thermal physiology traits will change by 50% the amount by
+          which the mean environmental temperature changes)")
+      )
+    )
+  ),
 
   # define sidebar layout
   sidebarLayout(
@@ -14,15 +30,15 @@ user_interface <- fluidPage(
     sidebarPanel(
 
       # add a numeric input for Topt
-      numericInput("Topt", "Thermal optima (Topt):",
+      numericInput("Topt", "Thermal optima (Topt, °C)",
                    30, min = 25, max = 35),
 
       # add a numeric input for CTmin
-      numericInput("CTmin", "Critical thermal minimum (CTmin):",
+      numericInput("CTmin", "Critical thermal minimum (CTmin, °C):",
                    15, min = 10, max = 20),
 
       # add a numeric input for CTmax
-      numericInput("CTmax", "Critical thermal maximum (CTmax):",
+      numericInput("CTmax", "Critical thermal maximum (CTmax, °C):",
                    40, min = 36, max = 45),
 
       # add a numeric input for Mean temperature
@@ -50,8 +66,11 @@ user_interface <- fluidPage(
     # add a main panel for displaying the plot
     mainPanel(
 
+      #set width (over 12)
+      width = 8,
+
       # add a plot output
-      plotOutput("plot")
+      plotOutput("plot", height = "600px"),
 
     )
 
@@ -61,6 +80,8 @@ user_interface <- fluidPage(
 
 # define server logic
 server_logic <- function(input, output){
+
+  library(tidyverse)
 
   output$plot <- renderPlot({
 
@@ -96,7 +117,7 @@ server_logic <- function(input, output){
     }
 
     # generate a sequence of temperatures
-    temp <- seq(0, 50, by = 0.1)
+    temp <- seq(0, 55, by = 0.1)
 
     # determine TPC without plasticity
     perf_without <- tpc(temp, input$Topt, input$CTmin, input$CTmax)
@@ -146,10 +167,56 @@ server_logic <- function(input, output){
     ggplot() +
       geom_line(data = TPC_data, aes(x = temp, y = perf, col = Plasticity, group = Plasticity),
                 linewidth = 2, alpha = 0.75) +
+      scale_color_manual(values = c("black", "orange")) +
       geom_area(data = Temp_data, aes(x = temp, y = 2*density, fill = distribution),
-                position = "identity", alpha = 0.25) +
-      geom_density(data = Perf_data, aes(y = perf, fill = plasticity), alpha = 0.5) +
-      scale_color_manual(values = c("orange", "black"))
+                position = "identity", alpha = 0.25, show.legend = FALSE) +
+      scale_fill_manual(values = c("darkgray", "darkred", "black", "orange")) +
+      geom_density(data = Perf_data, aes(y = perf, fill = plasticity),
+                   col = NA, alpha = 0.25, show.legend = FALSE) +
+      geom_vline(xintercept = input$Mean_Init + input$Mean_delta,
+                 linetype = "dashed", col = "darkred", linewidth = 1.5, alpha = 0.75) +
+      geom_segment(aes(x = 0, xend = input$Mean_Init + input$Mean_delta,
+                   y = mean(pred_perf_without), yend = mean(pred_perf_without)),
+                   col = "black", linewidth = 1.5, alpha = 0.75) +
+      geom_segment(aes(x = 0, xend = input$Mean_Init + input$Mean_delta,
+                       y = mean(pred_perf_with), yend = mean(pred_perf_with)),
+                   col = "orange", linewidth = 1.5, alpha = 0.75) +
+      scale_x_continuous(expand = c(0,0)) +
+      scale_y_continuous(limits = c(0,1.01), expand = c(0,0)) +
+      geom_point(aes(x = 45, y = 0.9), size = 10, col = "darkgray", shape = 15,
+                 alpha = 0.5, fill = "darkgray") +
+      geom_point(aes(x = 45, y = 0.85), size = 10, col = "darkred", shape = 15,
+                 alpha = 0.5, fill = "darkred") +
+      geom_text(aes(x = 47, y = 0.9, label = "Initial"),
+                size = 5, col = "black", hjust = 0) +
+      geom_text(aes(x = 47, y = 0.85, label = "After change"),
+                size = 5, col = "black", hjust = 0) +
+      geom_text(aes(x = 44, y = 0.95, label = "Temperature distribution"),
+                size = 5, col = "black",hjust = 0) +
+      geom_point(aes(x = 45, y = 0.75), size = 10, col = "black", shape = 22,
+                 alpha = 0.5, fill = "black") +
+      geom_point(aes(x = 45, y = 0.7), size = 10, col = "orange", shape = 22,
+                 alpha = 0.5, fill = "orange") +
+      geom_text(aes(x = 47, y = 0.75, label = "Absent"),
+                size = 5, col = "black", hjust = 0) +
+      geom_text(aes(x = 47, y = 0.7, label = "Present"),
+                size = 5, col = "black", hjust = 0) +
+      geom_text(aes(x = 44, y = 0.8, label = "Plasticity"),
+                size = 5, col = "black", hjust = 0) +
+      theme_minimal() +
+      theme(
+        axis.title = element_text(size = 20),
+        axis.text = element_text(size = 14),
+        axis.line = element_line(),
+        axis.ticks = element_line(),
+        legend.position = "none",
+        plot.title = element_text(size = 20)
+      ) +
+      xlab("Temperature (°C)") +
+      ylab("Performance") +
+      ggtitle(paste("Change in performance via plasticity: ",
+        round(mean(pred_perf_with) - mean(pred_perf_without), digits = 2),
+        sep = ""))
 
   })
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-"# tpc-plasticity-shiny" 
+# Exploring the effects of thermal plasticity on adaptation to climate change
+
+This application allows you to explore how can phenotypic plasticity in 
+thermal performance curves (TPC) aids adaptation to changing thermal environments.
+Adjust the thermal optima ($T_{opt}$) and critical thermal limits ($CT_{min}$ and $CT_{min}$ ) to
+shape the TPC. Set an initial temperature distribution (initial mean and standard deviation) 
+and explore how changes in mean and thermal variability (change in SD) impact performance.
+You can also control the TPC's acclimation response ratio ($ARR$) which will determine
+how much do all thermal physiology traits change as a function of changes in mean temperature 
+(e.g., $ARR$ = 0.5 will mean that all thermal physiology traits will change by 50% the amount by
+which the mean environmental temperature changes)
